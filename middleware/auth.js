9d6c3ad698e0eb3d69a8e946a15327ae15c4f614
KyleLavigne <112232301+KyleLavigne@users.noreply.github.com>
2025-05-11 17:26:28 -0500

    update again again

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,7 +10,7 @@ module.exports = async function (req, res, next) {
   try {
     const token = authHeader.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.id);
+    const user = await User.findById(decoded._id);
 
     if (!user) return res.status(401).json({ error: 'Invalid token' });
 
